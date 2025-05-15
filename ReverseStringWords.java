8e146f4c782525a45ffbf8064bc865f7da2bb907
Rishika <rishikajain2709@.com>
2025-05-15 21:57:24 +0530

    Strings theory and Advance Questions

---
+++ ReverseStringWords.java
@@ -0,0 +1,31 @@
+package Strings;
+
+public class ReverseStringWords {
+    public static void main(String[] args) {
+        String s="Hello hiee";
+        ////////  olleH eeih
+         StringBuilder sb=new StringBuilder();
+          String ans="";
+        for (int i = 0; i <s.length(); i++) {
+            char ch=s.charAt(i);
+            if(ch==' ')
+            {
+                sb.reverse();
+                //olleH
+                ans+=sb;
+                ans+=" ";
+                sb=new StringBuilder();
+                //olleH
+
+            }
+            else {
+                //olleHhiee
+                sb.append(ch);
+                //Hello
+            }
+        }
+        sb.reverse();
+        ans+=sb;
+        System.out.println(ans);
+    }
+}

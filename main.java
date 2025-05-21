57b24335a2e30da28864b6e3229e378c9530c15a
Rishika <rishikajain2709@.com>
2025-05-21 12:05:13 +0530

    Strings theory and Advance Questions

---
+++ main.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class main {
+    public  static void print(int sum)
+    {
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        int sum=a+b;
+        print(sum);
+    }
+}
